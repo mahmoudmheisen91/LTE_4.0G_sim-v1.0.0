78b65fe438967fd3ea22385ff7ac9f5fc1f18c49
Mahmoud Mheisen <mahmoudmheisen91@gmail.com>
2015-01-16 10:15:47 +0100

    Add simple max and min functions

--- Tests/example.c
+++ Tests/example.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sndfile.h>
+#include <math.h>
 #include "gnuplot_i.h"
 #include <signal.h>
 
 void catch_signal(int sig);
 void wait_for_ctrl_c(void);
 void plot_x(double *x, int length, char *style, char *xlabel, char *ylabel, char *title);
+double lte_adc(double *data, int length, int nbits,double *quantized_data,
+               double *encoded_data, double *error_signal);
+double max(double  *data, int length);
+double min(double  *data, int length);
 
 void main(void) {
 
@@ -21,10 +26,10 @@ void main(void) {
     int length = sf_read_double(sf1, data, info.frames);
     sf_close(sf1);
 
-    printf("%d\n", info.seekable);
+    printf("%.4f\n", min(data, length));
 
-    plot_x(data, length, "lines", "Time", "Amplitute", "Test Signal");
-    plot_x(data, length, "lines", "Time", "Amplitute", "Test Signal");
+    //plot_x(data, length, "lines", "Time", "Amplitute", "Test Signal");
+    //plot_x(data, length, "lines", "Time", "Amplitute", "Test Signal");
 
     // open new sound file and write to it:
     sf2 = sf_open("test_signal_2.wav", SFM_WRITE, &info);
@@ -63,3 +68,64 @@ void plot_x(double *x, int length, char *style, char *xlabel, char *ylabel, char
     // Close plot to remove tmp files:
     gnuplot_close(handler);
 }
+
+double lte_adc(double *data, int length, int nbits, double *quantized_data,
+               double *encoded_data, double *error_signal) {
+
+    // Parameters:
+    double data_max = max(data, length);
+    double data_min = min(data, length);
+    int nlevels = 2 ^ nbits;
+    double delta = (data_max - data_min) / (nlevels - 1);
+
+    // Function:
+    int i, j;
+    for(i = 0; i < length; i++) {
+        for(j = 0; j < nlevels; j++) {
+            if ((data[i] > data_min + delta * (j-1)) && (data[i] < data_min + delta * j)) {
+                if ((data_min + delta * (j-1)) > data_max)
+                    break;
+                quantized_data[i] =  data_min + delta * (j-1);
+                encoded_data[i] = j;
+            }
+        }
+    }
+
+    // Error:
+    error_signal     = data_quantized - data ;
+    %% SNR_dB:
+    var_data  = var(data);
+    var_error = var(error_signal);
+    SNR       = (var_data / var_error) ^ 2;
+    SNR_dB    = 10*log10(SNR);
+
+    return 0.0;
+}
+
+double max(double  *data, int length) {
+    int i;
+    double result = -999999.99;
+    for(i = 0; i < length; i++) {
+        if(data[i] > result)
+            result = data[i];
+    }
+    return result;
+}
+
+double min(double  *data, int length) {
+    int i;
+    double result = 999999.99;
+    for(i = 0; i < length; i++) {
+        if(data[i] < result)
+            result = data[i];
+    }
+    return result;
+}
+
+/*
+function [data_quantized,encoded_data,SNR_dB,Nlevels,error_signal] = LTE_ADC(data,Nbits)
+
+
+
+
+*/
