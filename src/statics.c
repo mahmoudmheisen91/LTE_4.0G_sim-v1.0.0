374fdf3ab56929788d6cf56ef3d930408e999725
Mahmoud Mheisen <mahmoudmheisen91@gmail.com>
2015-02-23 09:42:06 +0100

    modify mean and variance functions

--- src/statics.c
+++ src/statics.c
@@ -83,22 +83,63 @@ double max(double *data, int length) {
     return result;
 }
 
+/*-------------------------------------------------------------------------*/
+/**
+  @brief    find mean value of double pointer array
+  @param    data double array input
+  @param    length length of array
+  @return   mean value of array
+
+  Examples:
+
+  @code
+  double value = mean(data, 23);
+  @endcode
+
+ */
+/*--------------------------------------------------------------------------*/
+
 double mean(double *data, int length) {
-    int i;
+    // initial sum:
     double result = 0.0;
+
+    // summation:
+    int i;
     for(i = 0; i < length; i++)
         result += data[i];
 
+    // return:
     return result / length;
 }
 
-double var(double *data, int length) {
-    int i;
+/*-------------------------------------------------------------------------*/
+/**
+  @brief    find variance value of double pointer array
+  @param    data double array input
+  @param    length length of array
+  @return   variance of array
+
+  Examples:
+
+  @code
+  double value = variance(data, 23);
+  @endcode
+
+ */
+/*--------------------------------------------------------------------------*/
+
+double variance(double *data, int length) {
+    // inital sum:
     double result = 0;
+
+    // fin mean of array:
     double data_mean = mean(data, length);
 
+    // variance math:
+    int i;
     for(i = 0; i < length; i++)
         result += pow((data[i] - data_mean), 2);
 
+    // return:
     return result / (length - 1);
 }
